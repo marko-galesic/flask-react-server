346342da37ae118845b1fcc5198a41f33d2a48cb
root <root@ip-172-31-46-249.us-east-2.compute.internal>
2020-07-10 18:32:09 +0000

    Initial commit

---
+++ flaskserver/static/react/precache-manifest.991d1ff1535a0cc49513c7dd58d39851.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1abd3a628c7f31c23086902dbdb1801f",
+    "url": "/static/react/../../static/index.html"
+  },
+  {
+    "revision": "3ef3c2d9f5cce134f599",
+    "url": "/static/react/css/main.87a6f3c0.chunk.css"
+  },
+  {
+    "revision": "5cb986481cb177b6b8a2",
+    "url": "/static/react/js/2.d09ca467.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/react/js/2.d09ca467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ef3c2d9f5cce134f599",
+    "url": "/static/react/js/main.0165c2fb.chunk.js"
+  },
+  {
+    "revision": "00b90c6979e7b9785281",
+    "url": "/static/react/js/runtime-main.3a44c33a.js"
+  }
+]);
